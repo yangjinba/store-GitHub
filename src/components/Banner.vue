26890be6dc7b00e00bdf30cb79c5abab003500d2
杨金霸 <690373209@qq.com>
2020-08-28 17:25:19 +0800

    初始化banner组件

---
+++ src/components/Banner.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="banner">你好啊banner</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .banner{
+        width: 100%;
+        height: 200px;
+        background: red;
+    }
+</style>
